d647063ca99362346b5d03b46491089c12c3bc8f
raveen2001 <raveenanbarasan@gmail.com>
2023-02-25 22:19:22 +0530

    regex added

--- apps/shop-management/types/owner.ts
+++ apps/shop-management/types/owner.ts
@@ -4,7 +4,7 @@ import { RouteShorthandOptions } from "fastify";
 export const OwnerSchema = Type.Object({
   id: Type.String(),
   name: Type.String({ minLength: 3, maxLength: 50 }),
-  phone: Type.String({ minLength: 10, maxLength: 10 }),
+  phone: Type.String({ format: "regex", pattern: "^(\\+\\d{1,3}\\s)?\\d{10}$" }), // prettier-ignore
   email: Type.String({ format: "email" }),
   password: Type.String({ minLength: 8 }),
   image: Type.Union([Type.String(), Type.Null()]),
