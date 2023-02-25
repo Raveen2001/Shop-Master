a1f978c99f8846ac1c08474d3544e8bf35f53f01
raveen2001 <raveenanbarasan@gmail.com>
2023-02-25 18:05:17 +0530

    routes added

---
+++ apps/shop-management/routes/shop.ts
@@ -0,0 +1,15 @@
+import { FastifyPluginOptions } from "fastify";
+import FastifyTypebox from "../types/fastify";
+
+function shopPlugin(
+  fastify: FastifyTypebox,
+  options: FastifyPluginOptions,
+  next: Function
+) {
+  fastify.get("/", (req, reply) => {
+    reply.send("hello");
+  });
+  next();
+}
+
+export default shopPlugin;
