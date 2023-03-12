83dfc4aa3eaf651bba700e74a1d2fe0c8da253d3
raveen2001 <raveenanbarasan@gmail.com>
2023-03-12 21:14:51 +0530

    fonts added

--- apps/shop-master-ui/src/App.tsx
+++ apps/shop-master-ui/src/App.tsx
@@ -1,14 +1,14 @@
-import { Box, Button, useColorMode } from "ui";
+import { Box, Button, Typography, useColorMode } from "ui";
 import "./App.scss";
 
 function App() {
   const { toggleColorMode } = useColorMode();
   return (
     <Box className="App">
-
       <Button variant="contained" onClick={toggleColorMode}>
         Hello World
       </Button>
+      <Typography variant="h1">Hello</Typography>
     </Box>
   );
 }
