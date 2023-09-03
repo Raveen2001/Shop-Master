3a35fec3e04e5e36ad6be3040068a89110435196
raveen <raveenanbarasan@gmail.com>
2023-09-04 00:37:34 +0530

    sub-category types added

---
+++ apps/user-management/types/sub-category.ts
@@ -0,0 +1,51 @@
+import { Static, Type } from "@sinclair/typebox";
+import { OwnerSchemaWithoutPassword } from "./owner";
+import { optionalType } from "./utils";
+import { ShopSchemaOut } from "./shop";
+
+export const SubCategorySchema = Type.Object({
+  id: Type.String(),
+  name: Type.String({ minLength: 3, maxLength: 50 }),
+  image: optionalType(Type.String({ format: "uri" })),
+  createdAt: Type.String({ format: "date-time" }),
+  updatedAt: Type.String({ format: "date-time" }),
+  ownerId: Type.String(),
+  shopId: Type.String(),
+});
+
+export const SubCategorySchemaIn = Type.Omit(SubCategorySchema, [
+  "id",
+  "createdAt",
+  "updatedAt",
+]);
+
+export const SubCategorySchemaOut = Type.Intersect([
+  SubCategorySchema,
+  Type.Object({
+    shop: Type.Optional(ShopSchemaOut),
+    owner: Type.Optional(OwnerSchemaWithoutPassword),
+    // TODO: add products
+    // products: Type.Optional(Type.Array(EmployeeSchema)),
+  }),
+]);
+
+export type TSubCategorySchema = Static<typeof SubCategorySchema>;
+export type TSubCategoryIn = Static<typeof SubCategorySchemaIn>;
+export type TSubCategoryOut = Static<typeof SubCategorySchemaOut>;
+
+export const SubCategoryQueryParamSchema = Type.Object({
+  id: Type.String(),
+});
+
+export const SubCategoryQueryStringSchema = Type.Object({
+  includeOwner: Type.Boolean({ default: false }),
+  includeShop: Type.Boolean({ default: false }),
+  includeProducts: Type.Boolean({ default: false }),
+});
+
+export type TSubCategoryQueryParam = Static<typeof SubCategoryQueryParamSchema>;
+export type TSubCategoryQueryString = Static<
+  typeof SubCategoryQueryStringSchema
+>;
+
+export type TSubCategoryQueryByFields = "ownerId" | "shopId";
