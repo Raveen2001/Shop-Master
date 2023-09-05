e980abb098ecff046fa2756eca24202664032656
raveen <raveenanbarasan@gmail.com>
2023-09-05 22:58:51 +0530

    product variants types added

---
+++ apps/user-management/types/product-variant.ts
@@ -0,0 +1,43 @@
+import { Static, Type } from "@sinclair/typebox";
+
+export const ProductVariantSchema = Type.Object({
+  id: Type.String(),
+  productId: Type.String(),
+  name: Type.String({ minLength: 3, maxLength: 50 }),
+  acquiredPrice: Type.Number(),
+  salePrice: Type.Number(),
+  otherRetailerPrice: Type.Number(),
+  onlyForBilling: Type.Boolean({ default: false }),
+  createdAt: Type.String({ format: "date-time" }),
+  updatedAt: Type.String({ format: "date-time" }),
+  availability: Type.Boolean({ default: true }),
+  shopId: Type.String(),
+  ownerId: Type.String(),
+});
+
+export const ProductVariantSchemaIn = Type.Omit(ProductVariantSchema, [
+  "id",
+  "createdAt",
+  "updatedAt",
+]);
+
+export type TProductVariantSchema = Static<typeof ProductVariantSchema>;
+export type TProductVariantSchemaIn = Static<typeof ProductVariantSchemaIn>;
+
+export const ProductVariantQueryParamSchema = Type.Object({
+  id: Type.String(),
+});
+
+export const ProductVariantQueryStringSchema = Type.Object({
+  includeProduct: Type.Boolean({ default: false }),
+  includeShop: Type.Boolean({ default: false }),
+  includeOwner: Type.Boolean({ default: false }),
+});
+
+export type TProductVariantQueryParam = Static<
+  typeof ProductVariantQueryParamSchema
+>;
+
+export type TProductVariantQueryString = Static<
+  typeof ProductVariantQueryStringSchema
+>;
