a0b4da9038a55c86bcf92fa7794b48138c850615
raveen <raveenanbarasan@gmail.com>
2023-09-21 22:35:38 +0530

    exported ROW

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,9 +1,13 @@
+export { LoadingButton, TreeItem, TreeView } from "@mui/lab";
 export * from "@mui/material";
 export * from "@mui/x-date-pickers";
 export * from "./MyThemeProvider";
 export * from "./components";
 export * from "./hooks";
 export * from "./utils";
-export { LoadingButton, TreeView, TreeItem } from "@mui/lab";
 
-export { type ColumnDef, createColumnHelper } from "@tanstack/react-table";
+export {
+  createColumnHelper,
+  type ColumnDef,
+  type Row,
+} from "@tanstack/react-table";
