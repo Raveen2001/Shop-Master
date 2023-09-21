df70a8a170d92b2b685b0d162f95c18a8265acc9
raveen <raveenanbarasan@gmail.com>
2023-09-21 22:57:11 +0530

    updated table name

--- packages/ui/components/Table/index.tsx
+++ packages/ui/components/Table/index.tsx
@@ -1,4 +1,4 @@
-export { default as Table } from "./Table";
+export { default as ReactTable } from "./Table";
 export { default as PaginatedTable } from "./PaginatedTable";
 export { default as ReactQueryPaginatedTable } from "./ReactQueryPaginatedTable";
 
