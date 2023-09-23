3f2468e10f8574091cc8002004ee149f386f05a7
raveen <raveenanbarasan@gmail.com>
2023-09-23 15:17:29 +0530

    order options added

---
+++ apps/user-management/opts/order.ts
@@ -0,0 +1,44 @@
+import { RouteShorthandOptions } from "fastify";
+import {
+  OrderQueryParamSchema,
+  OrderQueryStringSchema,
+  OrderSchema,
+  OrderSchemaIn,
+} from "../types/order";
+import { Type } from "@sinclair/typebox";
+
+export const CreateOrderOpts: RouteShorthandOptions = {
+  schema: {
+    tags: ["Order"],
+    summary: "Create a new Order",
+    body: OrderSchemaIn,
+    querystring: OrderQueryStringSchema,
+    response: {
+      201: OrderSchema,
+    },
+  },
+};
+
+export const QueryOrderOpts: RouteShorthandOptions = {
+  schema: {
+    tags: ["Order"],
+    summary: "Get Order by order_id",
+    params: OrderQueryParamSchema,
+    querystring: OrderQueryStringSchema,
+    response: {
+      200: OrderSchema,
+    },
+  },
+};
+
+export const QueryOrdersByIdOpts: RouteShorthandOptions = {
+  schema: {
+    tags: ["Order"],
+    summary: "Get Orders by id",
+    params: OrderQueryParamSchema,
+    querystring: OrderQueryStringSchema,
+    response: {
+      200: Type.Array(OrderSchema),
+    },
+  },
+};
