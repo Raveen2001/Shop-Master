2a1b3dd1d677e2159077d2338d42439447269bc3
raveen <raveenanbarasan@gmail.com>
2023-09-23 15:04:16 +0530

    orders db schema added

---
+++ packages/database-drizzle/schema/order_items.ts
@@ -0,0 +1,41 @@
+import { relations } from "drizzle-orm";
+import { pgTable, uuid, integer } from "drizzle-orm/pg-core";
+import { ordersDB } from "./orders";
+import { productVariantsDB } from "./product_variants";
+
+export const orderItemsDB = pgTable("order_items", {
+  id: uuid("id").defaultRandom().primaryKey(),
+
+  orderId: uuid("order_id")
+    .notNull()
+    .references(() => ordersDB.id),
+
+  productVariantId: uuid("product_variant_id")
+    .notNull()
+    .references(() => productVariantsDB.id),
+
+  quantity: integer("quantity").notNull(),
+  unitPrice: integer("unit_price").notNull(),
+});
+
+export const ORDER_ITEMS_DB_COLUMNS = [
+  "id",
+  "orderId",
+  "productVariantId",
+  "quantity",
+  "unitPrice",
+] as const;
+
+export const orderItemsRelations = relations(orderItemsDB, ({ one }) => ({
+  order: one(ordersDB, {
+    fields: [orderItemsDB.orderId],
+    references: [ordersDB.id],
+  }),
+  productVariantsDB: one(productVariantsDB, {
+    fields: [orderItemsDB.productVariantId],
+    references: [productVariantsDB.id],
+  }),
+}));
+
+export type TOrderItemDB = typeof orderItemsDB.$inferSelect;
+export type TNewOrderItemDB = typeof orderItemsDB.$inferInsert;
---
+++ packages/database-drizzle/schema/orders.ts
@@ -0,0 +1,62 @@
+import { relations } from "drizzle-orm";
+import { pgEnum, pgTable, timestamp, uuid, serial } from "drizzle-orm/pg-core";
+import { ownersDB } from "./owners";
+import { shopsDB } from "./shops";
+import { customersDB } from "./customers";
+import { employeesDB } from "./employees";
+
+export const paymentTypeEnum = pgEnum("payment_type", ["CASH", "UPI", "CARD"]);
+
+export const ordersDB = pgTable("orders", {
+  id: serial("id").primaryKey(),
+  paymentType: paymentTypeEnum("type").notNull(),
+
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+
+  shopId: uuid("shop_id")
+    .notNull()
+    .references(() => shopsDB.id),
+  ownerId: uuid("owner_id")
+    .notNull()
+    .references(() => ownersDB.id),
+  customerId: uuid("customer_id").references(() => customersDB.id),
+  createdByEmployeeId: uuid("created_by_employee_id").references(
+    () => employeesDB.id
+  ),
+});
+
+export const ORDERS_DB_COLUMNS = [
+  "id",
+  "paymentType",
+  "createdAt",
+  "updatedAt",
+  "shopId",
+  "ownerId",
+  "customerId",
+  "createdByEmployeeId",
+] as const;
+
+export const ordersRelations = relations(ordersDB, ({ one }) => ({
+  shop: one(shopsDB, {
+    fields: [ordersDB.shopId],
+    references: [shopsDB.id],
+  }),
+  owner: one(ownersDB, {
+    fields: [ordersDB.ownerId],
+    references: [ownersDB.id],
+  }),
+  customer: one(customersDB, {
+    fields: [ordersDB.customerId],
+    references: [customersDB.id],
+  }),
+  createdByEmployee: one(employeesDB, {
+    fields: [ordersDB.createdByEmployeeId],
+    references: [employeesDB.id],
+  }),
+}));
+
+export type TOrderDB = typeof ordersDB.$inferSelect;
+export type TNewOrderDB = typeof ordersDB.$inferInsert;
+
+export const ORDER_PAYMENT_TYPES = paymentTypeEnum.enumValues;
