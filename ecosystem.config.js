29330bfa71d7b2809f5538a25b4a05d56584b63d
raveen-a <raveen.a@maersk.com>
2025-08-11 20:44:11 +0530

    Add cluster mode and instance configuration for management-api in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
       script: "npm start -w management-api",
       watch: false,
       name: "SHOP MASTER - management-api",
+      instances: "max",
+      exec_mode: "cluster",
       error_file: "./logs/management-api/error.log",
       out_file: "./logs/management-api/out.log",
       log_date_format: "YYYY-MM-DD HH:mm Z",
