caaf0a7f26a957f488d83b3eeb988dca4d5c1a44
raveen2001 <raveenanbarasan@gmail.com>
2025-10-19 10:22:27 +0530

    Enhance ecosystem configuration and update dependencies
    - Updated ecosystem.config.js to include new scripts for billing-frontend and shop-master-frontend services, improving project structure and deployment capabilities.
    - Added 'serve' as a new dependency in package.json and updated package-lock.json to reflect changes in the dependency tree, ensuring compatibility and stability.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,10 +14,30 @@ module.exports = {
       script:
         "npx turbo run build --filter=billing-api && npm start -w billing-api",
       watch: false,
-      name: "SHOP MASTER - billing",
+      name: "SHOP MASTER - billing-api",
 
       error_file: "./logs/billing/error.log",
       out_file: "./logs/billing/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+    },
+    {
+      script:
+        "npx turbo run build --filter=billing && npx serve -s apps/billing/dist -l 3001",
+      watch: false,
+      name: "SHOP MASTER - billing-frontend",
+
+      error_file: "./logs/billing-frontend/error.log",
+      out_file: "./logs/billing-frontend/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+    },
+    {
+      script:
+        "npx turbo run build --filter=shop-master-ui && npx serve -s apps/shop-master-ui/dist -l 3002",
+      watch: false,
+      name: "SHOP MASTER - shop-master-frontend",
+
+      error_file: "./logs/shop-master-frontend/error.log",
+      out_file: "./logs/shop-master-frontend/out.log",
       log_date_format: "YYYY-MM-DD HH:mm Z",
     },
   ],
